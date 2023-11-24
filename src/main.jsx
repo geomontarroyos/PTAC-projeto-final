b77dd72c6814a6f3eb99bd172ef5214bb4b5ec8c
geomontarroyos <gmontarroyosa@gmail.com>
2023-11-24 14:37:50 -0400

    commit
    Nesse primeiro commit fiz a estrutura, adicionando o cadastro, destaque e menu lateral e implementando um style.css para cada, por último adicionei as rotas das novas páginas criadas no main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
+import Home from './Home/Home';
+import Destaque from './Destaque/destaque';
+import Cadastro from './Cadastro/cadastro';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path='/destaque' element={<Destaque/>}></Route>
+        <Route path='/cadastro' element={<Cadastro/>}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
