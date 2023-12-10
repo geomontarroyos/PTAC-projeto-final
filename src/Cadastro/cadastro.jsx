7cc338bef4e8cf0ddf247c00c2721704d3c4e004
gmontarroyosa <gmontarroyosa@gmail.com>
2023-12-10 17:07:27 -0300

    commit

--- src/Cadastro/cadastro.jsx
+++ src/Cadastro/cadastro.jsx
@@ -2,40 +2,48 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-export default function ToDo() {
-    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"));
+export default function MusicaCadastro() {
+    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista")) || [];
     const [id, setId] = useState(listaLocalStorage[listaLocalStorage.length - 1]?.id + 1 || 1);
-    const [lista, setLista] = useState([]);
-    const [autor, setAutor] = useState("");
-    const [livro, setLivro] = useState("");
-    const [genero, setGenero] = useState("");
- 
-
-   //useEffect(() => {localStorage.setItem("Lista", JSON.stringify(lista))}, [lista]);
-   useEffect(() => {
-    const listaJSON = localStorage.getItem("Lista");
-    if (listaJSON) {
-    const listaArmazenada = JSON.parse(listaJSON);
-    setLista(listaArmazenada);
-  }
-}, []);
-    useEffect(() => {
-      localStorage.setItem("Lista", JSON.stringify(lista));
-    }, [lista]);
+    const [nome, setNome] = useState("");
+    const [cantor, setCantor] = useState("");
+    const [estilo, setEstilo] = useState("");
+    const [gravadora, setGravadora] = useState("");
+    const [dataLancamento, setDataLancamento] = useState("");
+    const [link, setLink] = useState("");
 
 
-    const salvar = (e) => {
+
+   useEffect(()=> {localStorage.setItem("Lista", JSON.stringify(lista))}, [lista]);
+
+   const navagate = useNavigate()
+
+
+    const dadosSalvos = async (e) => {
       e.preventDefault();
-      setLista([
+
+       await setLista([
         ...lista,
         {
           id: id,
-          autor: autor,
-          livro: livro,
-          genero: genero,
-        },
+          nome: nome,
+          cantor: cantor,
+          estilo: estilo,
+          gravadora: gravadora,
+          dataLancamento: dataLancamento,
+          link: link
+        }
       ]);
-      setId(id + 1);
+
+      navagate("/");
+      setId(id+1);
+      setNome("");
+      setCantor("");
+      setEstilo("");
+      setGravadora("");
+      setDataLancamento("");
+      setLink("");
+
     };
    
     const remover = (id) => {
@@ -51,25 +59,42 @@ export default function ToDo() {
   
     return (
         <div class="container">
-      <h1>Lista Biblioteca</h1>
-      <form onSubmit={salvar}>
+      <h1>Cadastro das Melhores Musicas</h1>
+      <form onSubmit={dadosSalvos}>
 
-         <h3>Digite seu autor a ser adicionado:</h3> <input type="text"
-              value={autor}
-              onChange={(e) => { setAutor(e.target.value) }} />
+         <h3>Digite sua musica a ser adicionada:</h3>
+          <input type="text" 
+            value={nome}
+            onChange={(e) => { setNome(e.target.value) }}/>
+          
+          <h3>Digite o cantor da musica adicionada:</h3>
+          <input type="text" 
+            value={cantor}
+            onChange={(e) => { setCantor(e.target.value) }}/>
 
-       <h3> Digite seu livro a ser adicionado:</h3><input type="text"
-              value={livro}
-              onChange={(e) => { setLivro(e.target.value) }} />
+          <h3>Digite o estilo da musica adicionada:</h3>
+          <input type="text" 
+            value={estilo}
+            onChange={(e) => { setEstilo(e.target.value) }}/>
 
-      <h3>Digite seu genero a ser adicionado:</h3><input type="text"
-              value={genero}
-              onChange={(e) => { setGenero(e.target.value) }} />
+          <h3>Digite a gravadora da musica adicionada:</h3>
+          <input type="text" 
+            value={gravadora}
+            onChange={(e) => { setGravadora(e.target.value) }}/>
 
-              <br />
-              <br />
+          <h3>Digite data de lançamento da musica a ser adicionada:</h3>
+          <input type="text" 
+            value={dataLancamento}
+            onChange={(e) => { setDataLancamento(e.target.value) }}/>  
 
-          <button>ADICIONAR</button>
+          <h3>Digite o link da musica a ser adicionada:</h3>
+          <input type="text" 
+            value={link}
+            onChange={(e) => { setLink(e.target.value) }}/> 
+
+       
+
+          <button>Adicionar nova música</button>
       </form>
 
       {lista.map((ativ) =>
@@ -84,11 +109,11 @@ export default function ToDo() {
               </li>
               <li>
                   Genero:<p>{ativ.genero}</p>
-                  <button onClick={() => remover(ativ.id)}>Remover</button>
+                  
               </li>
           </ul>
       )}
-      <br />
+     <button onClick={() => remover(ativ.id)}>Remover</button>
      <button> <Link to="/">Voltar para a página inicial</Link> </button>
   </div>
 );
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import Destaque from './Destaque/destaque.jsx';
 import Cadastro from './Cadastro/cadastro.jsx';
@@ -11,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path='/destaque' element={<Destaque/>}></Route>
+        <Route path='/detalhes/:id' element={<Detalhes/>}></Route>
         <Route path='/cadastro' element={<Cadastro/>}></Route>
       </Routes>
     </BrowserRouter>
