88e82dc11e4dcbf3c36db555d153c70ea2039547
geomontarroyos <gmontarroyosa@gmail.com>
2023-12-11 23:11:52 -0300

    commit
    Ajeite as frases contidas no formulário e retirei o botão de remover já que o senhor não pediu para fazer e eu não estava dando conta.

--- src/Cadastro/cadastro.jsx
+++ src/Cadastro/cadastro.jsx
@@ -22,14 +22,14 @@ export default function Cadastro() {
     e.preventDefault();
 
      setLista([...lista, {
-      id: id,
       nome: nome,
       cantor: cantor,
       estilo: estilo,
       gravadora: gravadora,
       dataLancamento: dataLancamento,
       link: link,
-      curtidas: curtidas
+      curtidas: curtidas,
+      id: id,
      }]);
       
     
@@ -43,78 +43,56 @@ export default function Cadastro() {
     setCurtidas("");
   };
 
-  const removerdados = (id) => {
-    const auxLista = lista.filter((remover) => remover.id !== id);
-    setLista(auxLista);
-  };
-
   return (
     <div className="container">
       <h1>Cadastro das Melhores Músicas</h1>
       <form onSubmit={dadosSalvos}>
 
-          <h3>Digite o ID da música a ser adicionada:</h3>
-          <input type="text" 
-            value={id}
-            onChange={(e) =>  setId(e.target.value) }/>
-
-         <h3>Digite sua musica a ser adicionada:</h3>
+         <h3>Digite sua musica da musica:</h3>
           <input type="text" 
             value={nome}
             onChange={(e) =>  setNome(e.target.value) }/>
           
-          <h3>Digite o cantor da musica adicionada:</h3>
+          <h3>Digite o cantor:</h3>
           <input type="text" 
             value={cantor}
             onChange={(e) =>  setCantor(e.target.value) }/>
 
-          <h3>Digite o estilo da musica adicionada:</h3>
+          <h3>Digite o estilo da musica:</h3>
           <input 
             type="text" 
             value={estilo} 
             onChange={(e) => setEstilo(e.target.value)} />
 
-          <h3>Digite a gravadora da musica adicionada:</h3>
+          <h3>Digite a gravadora da musica:</h3>
           <input type="text" 
             value={gravadora}
             onChange={(e) =>  setGravadora(e.target.value) }/>
 
-          <h3>Digite data de lançamento da musica a ser adicionada:</h3>
+          <h3>Digite data de lançamento da musica:</h3>
           <input type="text" 
             value={dataLancamento}
             onChange={(e) =>  setDataLancamento(e.target.value) }/>
 
-          <h3>Digite a quantidade de curtidas a  música a ser adicionada tem :</h3>
+          <h3>Digite a quantidade de curtidas da  música:</h3>
           <input type="text" 
             value={curtidas}
             onChange={(e) =>  setCurtidas(e.target.value) }/>   
 
-          <h3>Digite o link da musica a ser adicionada:</h3>
+          <h3>Digite o link da musica:</h3>
           <input type="text" 
             value={link}
             onChange={(e) => setLink(e.target.value) }/> 
 
+            
+          <h3>ID:</h3>
+          <input type="text" 
+            value={id}
+            onChange={(e) =>  setId(e.target.value) }/>
+
           <button>Adicionar nova música</button>
       </form>
-      
-
-      {lista.map((remover) => (
-        <ul key={remover.id}>
-          <Link to={`/detalhes/${remover.id}`}>
-            <li>
-              Nome:<p>{remover.nome}</p>
-            </li>
-            <li>
-              Cantor:<p>{remover.cantor}</p>
-            </li>
-            <li>
-              Estilo:<p>{remover.estilo}</p>
-            </li>
-            <button onClick={() => removerdados(remover.id)}>Remover</button>
-          </Link>
-        </ul>
-      ))}
-
+    
       <button>
         <Link to="/">Voltar para a página inicial</Link>
       </button>
